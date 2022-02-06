ca093179a5a6bde6f22a660035bd7037652f8924
xjenekx <xxjenekxx@gmail.com>
2022-02-06 23:38:32 +0300

    - candles: /api/v1/candles/ -> /api/v1/candles_by_full_market_name/

--- src/api/fetchCandlesForLast60Days.ts
+++ src/api/fetchCandlesForLast60Days.ts
@@ -4,14 +4,14 @@ import { API_HOST } from "../../config";
 import { ICandles } from "@/interfaces/candles.inerface";
 
 export default async function fetchCandlesForLast60Days(
-  market_name: string
+  full_market_name: string
 ): Promise<ICandles[]> {
   const date = new Date();
   const end = date.toISOString();
   date.setUTCDate(date.getUTCDate() - 60);
   const start = date.toISOString();
   const { data } = await axios.get(
-    `${API_HOST}/api/v1/candles/${market_name}?period=daily&start=${start}&end=${end}`
+    `${API_HOST}/api/v1/candles_by_full_market_name/${full_market_name}?period=daily&start=${start}&end=${end}`
   );
   return data;
 }
